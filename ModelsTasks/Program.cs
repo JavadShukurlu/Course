0ce955d1d5e5c734ea7d963359c1ec130431237c
cavad <sukurlucavad@gmail.com>
2026-03-16 13:28:12 +0400

    teacher hazir

--- ModelsTasks/Program.cs
+++ ModelsTasks/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ModelsTasks;
+
+Student student = new Student("Cavad","Sukurlu","Cavad","APA103",98,new DateOnly(2005,11,18),"Baku","0553316415","sukurlucavad@gmail.com","123456");
+
+Console.WriteLine(student.GetName());
+Console.WriteLine(student.GetGroup());
+Console.WriteLine(student.GetStudent());
+Console.WriteLine(student.GetEmail());
+Console.WriteLine(student.GetBitdate());
+Console.WriteLine(student.GetContact());
