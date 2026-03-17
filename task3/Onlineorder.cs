bd7dfa4e4934c0a40d032549851e60e897a06e11
cavad <sukurlucavad@gmail.com>
2026-03-17 13:58:54 +0400

    student

---
+++ task3/Onlineorder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Onlineorder:Order
+    {
+        public double distance;
+        public Onlineorder(int id, string name, int Count, double price,double distance):base(id,name,Count,price)
+        {
+            this.distance = distance; 
+        }
+    }
+}
---
+++ task3/Order.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Order
+    {
+        public int id;
+        public string name;
+        public int Count;
+        public double price;
+
+        public Order(int id, string name, int Count, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.Count = Count;
+            this.price = price;
+        }
+        public Order(int id, string name, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.Count = 1;
+            this.price = price;
+        }
+        public double TotalPrice()
+        {
+            return Count * price;
+        }
+        public void Incrise(int n)
+        {
+            Count += n;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"adi:{name}, Qiymet{price}, Miqdar{Count}, UmumiQiymet{TotalPrice()}");
+        }
+    }; 
+}
